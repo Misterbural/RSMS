0b1b65eabb7a17003144adc71a3ea47ea39e6ba9
Tony LAUNAY <tony.launay@ingesup.com>
2015-05-11 13:40:51 +0200

    Changed CSS into login view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150505085230) do
+ActiveRecord::Schema.define(version: 20150505095820) do
 
   create_table "commands", force: :cascade do |t|
     t.string   "name",       limit: 25,  null: false
