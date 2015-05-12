73867d56e27fcc6cd32811c0aef5ba82f33587ec
Eliott Bureau <eliottbureau@gmail.com>
2015-05-12 10:33:30 +0200

    fix migration

--- db/migrate/20150511141545_create_groups_scheduleds.rb
+++ db/migrate/20150511141545_create_groups_scheduleds.rb
@@ -1,10 +1,8 @@
 class CreateGroupsScheduleds < ActiveRecord::Migration
   def change
     create_table :groups_scheduleds do |t|
-      t.integer :scheduled_id
-      t.integer :group_id
-
-      t.timestamps null: false
+      t.belongs_to :scheduled
+      t.belongs_to :group
     end
   end
 end
--- db/migrate/20150511141651_create_numbers_scheduleds.rb
+++ db/migrate/20150511141651_create_numbers_scheduleds.rb
@@ -1,7 +1,7 @@
 class CreateNumbersScheduleds < ActiveRecord::Migration
   def change
     create_table :numbers_scheduleds do |t|
-      t.integer :scheduled_id
+      t.belongs_to :scheduled, index:true
       t.string :number, limit: 12
 
       t.timestamps null: false
